3b72cf3f93a2d91f3d55e2db6aca1c3a6de7dcba
dyk <dyk@dykdeMacBook-Pro.local>
2020-02-11 22:32:49 +0800

    feat: add vue-rest-hooks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,22 @@
 import { createComponent, onMounted } from "@vue/composition-api";
+import { useQuery } from "vue-rest-hooks";
 
 export default createComponent({
   setup() {
+    const getToken = (params: { name: string }): Promise<{ name: string }> =>
+      new Promise(resolve => {
+        resolve(params);
+      });
+
+    const { loading, data, error } = useQuery<
+      { name: string },
+      { name: string }
+    >(getToken as any, {
+      variables: {
+        name: "123123"
+      }
+    });
+
     onMounted(() => {
       console.log("hello home");
     });
