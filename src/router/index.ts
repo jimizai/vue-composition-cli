a3be0b7e0eb02e763179af1a40823732d7e00c22
jimizai <359743984@qq.com>
2020-02-20 17:00:36 +0800

    feat: add router guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import store from "../store";
 import appsRouter from "./modules/apps";
 
 Vue.use(VueRouter);
@@ -22,8 +22,23 @@ const routes = [
   }
 ];
 
+let user: any = localStorage.getItem("user");
+if (user) {
+  //do login
+}
+
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach((to, _, next) => {
+  if (to.matched.some(r => r.meta.requiresAuth)) {
+    // router guard
+  } else {
+    next();
+  }
+});
+
+router.afterEach(to => {});
 
 export default router;
